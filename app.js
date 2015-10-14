780e737eb3d1cf711c0f9c297eb583de050a7ca4
Antonio Sejas <antonio@sejas.es>
2015-10-14 17:16:04 +0100

    app.js example

--- app.js
+++ app.js
@@ -1,60 +1,50 @@
+
+// Module dependencies
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+var morgan = require('morgan');
+var flash = require('connect-flash');
+var multiparty = require('connect-multiparty');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var config = require('./config.json');
+var routes = require('./routes');
+var db = require('./lib/db');
+// View setup
 app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.locals = require('./helpers/index');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+// Loading middleware
+   app.use(morgan('dev'));
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({ extended: true }));
+   app.use(methodOverride(function(req, res){
+     if (req.body && typeof req.body === 'object' && '_method' in req.
+   body) {
+       // look in url - encoded POST bodies and delete it
+       var method = req.body._method;
+       delete req.body._method;
+       return method;
+} }));
+   app.use(cookieParser());
+   app.use(cookieSession({
+     secret: config.sessionSecret,
+     cookie: {
+       maxAge: config.sessionMaxAge
+     }
+   }));
+   app.use(flash());
+   if (app.get('env') === 'development') {
+     app.use(errorHandler());
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+   // static middleware after the routes
+   app.use(express.static(__dirname + '/public'));
+   // Establishing database connection and binding application to
+   specified port
+   db.connect();
+   app.listen(config.port);
+   console.log('listening on port %s', config.port);
